e284a70669d7ea6fe3b919cc904920dcdcc7705c
Aaron Dai <aaron.dai@nokia-sbell.com>
2025-12-30 11:01:18 +0800

    fix: 更新 ecosystem.config.js 使用 standalone server 模式

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   apps: [{
     name: 'alens_portfolio',
-    script: 'npm',
-    args: 'start',
-    cwd: '/deploy/alens_portfolio',  // 添加这行，项目绝对路径	  
+    script: 'node',
+    args: '.next/standalone/server.js',
+    cwd: '/deploy/alens_portfolio',
     env: {
       NODE_ENV: 'production',
       PORT: 3000
